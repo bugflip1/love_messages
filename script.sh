c8e9b79f46b9fd52de73ccee3af0d171651cac13
Jonathan Felix <ferrule-resort0i@icloud.com>
2025-02-24 19:37:36 -1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Array of messages
-messages =( "test" "hi" )
+messages=("test" "hi")
+
 # Get a random message
 random_message=${messages[RANDOM % ${#messages[@]}]}
 
+# Print message for xbar
 echo "$random_message"
